6b2a50b946ed35f1e2c1ba36d74e63051011f678
Amir Ridwan <amirridwan.dev@gmail.com>
2026-02-21 14:45:38 +0800

    fix: styling marketplace

--- src/features/marketplace/Marketplace.tsx
+++ src/features/marketplace/Marketplace.tsx
@@ -59,7 +59,7 @@ export const Marketplace = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 pt-20 pb-16">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="mb-12">
             
         </div>
--- src/features/marketplace/components/product-card.tsx
+++ src/features/marketplace/components/product-card.tsx
@@ -1,36 +1,38 @@
 
 import { FaShoppingCart } from 'react-icons/fa'
 import { formatPrice } from '../../../util/price'
-import type { Product } from '../types'
+import type { Product } from '../../../types/product/product'
 
 interface ProductCardProps {
   product: Product
 }
 
 export const ProductCard = ({ product }: ProductCardProps) => {
+
+  const handleAddToCart = () => {
+    console.log('Add to cart')
+  }
   return (
     <div className="group relative bg-white rounded-lg border border-gray-200 overflow-hidden transition-all duration-300 hover:shadow-lg hover:border-gray-300">
-      <div className="p-6">
-        <div className="flex items-start justify-between mb-3">
+      <div className="flex flex-col gap-4 p-6">
+        <div className="flex items-start justify-between">
           <div className="flex-1">
             <h3 className="text-lg font-semibold text-gray-900 mb-1 group-hover:text-gray-700 transition-colors">
               {product.name}
             </h3>
-            <p className="text-sm text-gray-500">{product.shop.name}</p>
+            <p className="text-sm text-gray-500">{product.shop?.name}</p>
           </div>
         </div>
-        
-        {product.description && (
-          <p className="text-sm text-gray-600 mb-4 line-clamp-2">
-            {product.description}
-          </p>
-        )}
-        
-        <div className="flex items-center justify-between pt-4 border-t border-gray-100">
-          <span className="text-2xl font-bold text-gray-900">
+
+        <div className="w-full h-48 object-cover rounded-lg overflow-hidden">
+          <img src={product.image_url} alt={product.name} className="size-full object-cover" />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <span className="text-xl font-bold text-gray-900">
             {formatPrice(product.price)}
           </span>
-          <button className="flex items-center gap-1 px-4 py-2 text-gray-900 text-sm font-medium rounded-md hover:bg-gray-800 transition-colors">
+          <button className="flex items-center gap-1 px-4 py-2 text-gray-900 text-sm font-medium rounded-md border border-gray-300 shadow-sm hover:bg-gray-800 transition-colors ">
             + <FaShoppingCart className='size-4' />
           </button>
         </div>
